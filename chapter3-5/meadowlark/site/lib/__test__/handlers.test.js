c94c76de6ccf897a1034807258ab35595810c90f
david.crespo <david.crespo@hmcts.net>
2019-12-29 12:50:18 +0100

    Add unit test for the about handler

--- chapter3-5/meadowlark/site/lib/__test__/handlers.test.js
+++ chapter3-5/meadowlark/site/lib/__test__/handlers.test.js
@@ -8,6 +8,21 @@ test('home page render', () => {
 
     handlers.home(req, res)
 
-    expect(res.render.mock.calls.length).toBe(1);
-    expect(res.render.mock.calls[0][0]).toBe('home');
+    expect(res.render.mock.calls.length).toBe(1)
+    expect(res.render.mock.calls[0][0]).toBe('home')
+})
+
+test('about page render', () => {
+    const res = {
+        render: jest.fn()
+    }
+    const req = {}
+
+    handlers.about(req, res)
+
+    expect(res.render.mock.calls.length).toBe(1)
+    expect(res.render.mock.calls[0][0]).toBe('about')
+    expect(res.render.mock.calls[0][1]).toEqual(expect.objectContaining({
+        fortune: expect.any(String)
+    }))
 })
